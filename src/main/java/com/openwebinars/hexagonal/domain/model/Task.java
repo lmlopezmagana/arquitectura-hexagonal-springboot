2e80d63fa2fa1932c25c6ca162392033ff94de54
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-13 22:55:18 +0200

    2.1 Task

---
+++ src/main/java/com/openwebinars/hexagonal/domain/model/Task.java
@@ -0,0 +1,25 @@
+package com.openwebinars.hexagonal.domain.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Setter(AccessLevel.NONE)
+    private TaskId id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private boolean completed;
+
+    public void taskCompleted() {
+        this.completed = true;
+    }
+
+
+}
+
