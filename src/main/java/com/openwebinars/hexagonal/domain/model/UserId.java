3ab7d907dbcf05fa001e9b3ce1070323d79b5f0c
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-14 12:38:15 +0200

    3.1 Dominio - User, UserRepository

---
+++ src/main/java/com/openwebinars/hexagonal/domain/model/UserId.java
@@ -0,0 +1,20 @@
+package com.openwebinars.hexagonal.domain.model;
+
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+public class UserId {
+
+    UUID value;
+
+    public static UserId of(UUID id) {
+        return new UserId(id);
+    }
+
+    public static UserId of(String id) {
+        return new UserId(UUID.fromString(id));
+    }
+
+}
