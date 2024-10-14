38e82d7ae069d4047250684a1ef92c14d407c5db
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-14 11:27:54 +0200

    2.5 Infrastructure - Adapter TaskRepository

---
+++ src/main/java/com/openwebinars/hexagonal/infrastructure/db/repos/impl/TaskRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.openwebinars.hexagonal.infrastructure.db.repos.impl;
+
+import com.openwebinars.hexagonal.domain.model.Task;
+import com.openwebinars.hexagonal.domain.model.TaskId;
+import com.openwebinars.hexagonal.domain.repository.TaskRepository;
+import com.openwebinars.hexagonal.infrastructure.db.entity.TaskEntity;
+import com.openwebinars.hexagonal.infrastructure.db.repos.jpa.TaskEntityRepositoryJpa;
+import com.openwebinars.hexagonal.infrastructure.mapper.TaskMapper;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class TaskRepositoryImpl implements TaskRepository {
+
+    private final TaskEntityRepositoryJpa taskEntityRepositoryJpa;
+
+    @Override
+    public Task save(Task task) {
+        TaskEntity entity = TaskMapper.toPersistence(task);
+        return TaskMapper.toDomain(taskEntityRepositoryJpa.save(entity));
+    }
+
+    @Override
+    public List<Task> getAll() {
+        return taskEntityRepositoryJpa.findAll()
+                .stream()
+                .map(TaskMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public Optional<Task> getById(TaskId id) {
+        return taskEntityRepositoryJpa.findById(id.getValue())
+                .map(TaskMapper::toDomain);
+    }
+
+    @Override
+    public void delete(TaskId id) {
+        taskEntityRepositoryJpa.deleteById(id.getValue());
+    }
+
+
+}
